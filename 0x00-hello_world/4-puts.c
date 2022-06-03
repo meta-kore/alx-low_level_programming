4f2f1e221cdb454d40fc444ea1f644cd66fb1a35
meta-kore <79240412=meta-kore@users.noreply.github.com>
2022-06-03 12:32:44 -0700

    my c code commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,18 +2,14 @@
 
 /**
  * My first bit of code
- * Think I like this
  *   
  * Going to stick around
- * Do hard things
  */
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }
