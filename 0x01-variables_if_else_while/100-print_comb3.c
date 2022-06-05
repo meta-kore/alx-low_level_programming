dba424c5db6815efcc275ff8e36ce229dab72f93
meta-kore <79240412=meta-kore@users.noreply.github.com>
2022-06-05 07:00:01 -0700

    if else while-101

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main prints combinations between 00 to 89
+ * Returns: 0
+ */
+int main(void)
+{
+	int i, a;
+
+	i = 48;
+	a = 48;
+
+	while (a < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			if (a != i && a < i)
+			{
+				putchar(a);
+				putchar(i);
+				if (i == 57 && a == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
