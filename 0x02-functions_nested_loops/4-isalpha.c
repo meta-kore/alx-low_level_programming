f0e7addaaba74dfd721a79be995dc97b8864aa6b
meta-kore <79240412=meta-kore@users.noreply.github.com>
2022-06-08 10:43:08 -0700

    is alpha bad

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,13 @@
 #include "main.h"
 
 /**
- * int _isalpha is a prototype function that.
+ * main - int _isalpha is a prototype function that.
  * _isalpha - shows 1 if the input is a letter, otherwise shows 0.
- *
  * @c: The character in ASCII code
  *
  * Return: 1 for letters. 0 for the rest.
  */
-
 int _isalpha(int c)
-
-	/* int _isalpha(int c) checks for lowercase */
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
