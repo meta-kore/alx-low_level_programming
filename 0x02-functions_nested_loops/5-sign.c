0d7db5b3a69f7e3135efce2c21574595b5dbeabb
meta-kore <79240412=meta-kore@users.noreply.github.com>
2022-06-08 11:01:28 -0700

    sign putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,12 +10,12 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(45);
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(43);
+		_putchar(45);
 		return (-1);
 	}
 	else
@@ -24,5 +24,4 @@ int print_sign(int n)
 		return (0);
 	}
 	_putchar('\n');
-}
-	
+}	
