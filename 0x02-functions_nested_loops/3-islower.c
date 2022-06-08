b32612a3890475592bd299763bc9a15ec3b7fe68
meta-kore <79240412=meta-kore@users.noreply.github.com>
2022-06-08 10:03:13 -0700

    is lower than

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
+ * main -int _islowerchecks for lowercase
  * int _islower(int c) is a prototype function
- * _islower - Shows 1 if the input is lowercase character.
+ * _islower: Shows 1 if the input is lowercase character.
  * @c: The character in ASCII code
  * Return: 1 for lowercase character. 0 for the rest.
  */
