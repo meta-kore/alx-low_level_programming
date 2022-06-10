b620951af6193fae753799b836b9186767c1be39
meta-kore <79240412+meta-kore@users.noreply.github.com>
2022-06-10 05:36:32 -0700

    debug hell

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * positive_or_negative - tests function that prints if 
+ * main - positive_or_negative - tests function that prints if 
  * integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
