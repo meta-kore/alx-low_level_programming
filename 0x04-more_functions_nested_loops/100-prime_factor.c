d71568a6c2aa9676b30c3fe665b12977d5d48aaa
meta-kore <79240412+meta-kore@users.noreply.github.com>
2022-06-10 19:16:28 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime number
+ * Return: zero
+ */
+{
+	long int n, fp;
+
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
+	{
+		if (n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
+	}
+	printf("%ld\n", fp);
+	return (0);
+}
