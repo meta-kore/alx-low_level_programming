8c2cdda1c54b3f6c91ccf4441e25140402fe834c
meta-kore <79240412+meta-kore@users.noreply.github.com>
2022-06-10 18:44:16 -0700

    more nests

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_triangle - this prints a triangle.
+ * @size: number of object lines
+ * Return: nil
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
