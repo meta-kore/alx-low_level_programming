c59db2e124b5044a4613d742bc9341bc957d942d
meta-kore <79240412+meta-kore@users.noreply.github.com>
2022-07-15 03:07:17 -0700

    prints elements of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_List - prints all the elements of a list_t list.
+ * print_list - Prints all the elements of a list_t list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
