dfc0cf0a9563d725d028b98cec3d0e4d7375dc41
meta-kore <79240412+meta-kore@users.noreply.github.com>
2022-12-17 03:55:37 +0300

    more looping and nesting

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/*
  * _isupper - checks if parameter is an uppercase character.
  * Proto: type
  * @c: input character
@@ -10,6 +10,7 @@
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+
 	{
 		return(1);
 	}
